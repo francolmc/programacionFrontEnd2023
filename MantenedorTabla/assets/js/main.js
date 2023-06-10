ef219aad27c62dda2ff2f33194ef55cf4b9c7902
Franco Morales Castro <franco.morales@outlook.com>
2023-06-09 22:20:53 -0400

    feat: ✨ mantenedor lista de usuarios

--- MantenedorTabla/assets/js/main.js
+++ MantenedorTabla/assets/js/main.js
@@ -16,20 +16,59 @@ class Person {
 
 // Contar un metodo que muestre la lista de datos en la tabla HTML
 const showData = () => {
-  personList.push(new Person("prueba@prueba.cl", "Prueba", "Prueba2"));
-
   // obtener la tabla del documento HTML
-  const tableHTML = document.getElementById('personlist');
-    console.log(tableHTML);
-  // limpiar contenido de la tabla
-  tableHTML.innerHTML = "";
+  const objectTableHTML = document.getElementById('personlist');
+  // limpiar contenido de la tabla y asignar las cabeceras
+  let tableHTML = `<tr>
+                      <th>Email</th>
+                      <th>Nombre</th>
+                      <th>Apellidos</th>
+                      <th></th>
+                    </tr>`;
+  // crear las filas para la tabla HTML con los objetos del arreglo
   for (let i = 0; i < personList.length; i++) {
-    const rowTableHTML = document.createElement("tr");
-    const colTableHTML = document.createElement("td");
-    colTableHTML.classList.add(this.personList[i].email);
-    rowTableHTML.appendChild(colTableHTML);
-    tableHTML.appendChild(rowTableHTML);
+    tableHTML = tableHTML + `<tr>
+                              <td>${personList[i].email}</td>
+                              <td>${personList[i].firstName}</td>
+                              <td>${personList[i].lastName}</td>
+                              <td><a href="#">Editar</a> | 
+                              <a href="#" onclick="deletePerson('${personList[i].email}')">Eliminar</a></td>
+                            </tr>`
   }
+  // cargar el string con el HTML de la tabla a la tabla del documento
+  objectTableHTML.innerHTML = tableHTML;
 };
 
+// funcion para agregar personas al arreglo global
+const addPerson = () => {
+  // validacion de dato unico
+  if (personList.findIndex((item) => item.email === document.getElementById('email').value)>=0)
+    alert('No se puede ingresar un correo existente.');
+  else {
+    // definir objeto persona a partir de la clase
+    const person = new Person();
+    person.email = document.getElementById('email').value;
+    person.firstName = document.getElementById('firstName').value;
+    person.lastName = document.getElementById('lastName').value;
+    personList.push(person);
+  }
+  // lamada al metodo que llena la tabla HTML para que aparezca el nuevo dato
+  showData();
+};
+
+// funcion para eliminar datos del arreglo
+const deletePerson = (email) => {
+  // funcion javascript para confirmar la eliminacion
+  if(confirm('Esta seguro de eliminar el registro?')) {
+    // buscar indice del dato
+    const indexPerson = personList.findIndex((item) => item.email === email);
+    // eliminar dato del arreglo
+    personList.splice(indexPerson, 1);
+  }
+  // llamar a la funcion que dibuja o renderiza la tabla en el HTML
+  showData();
+};
+
+
+// llamada inicial a la metodo que llena la tabla HTML con el arreglo de objetos
 showData();
